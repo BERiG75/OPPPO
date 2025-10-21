131dd9e14e9831764ea64dc3c573fe309b33e524
BERiG75 <berdinigor25@gmail.com>
2025-10-21 17:18:40 +0700

    Formatter warnings fixed

--- main.py
+++ main.py
@@ -5,130 +5,131 @@ from stationery_classes import Paper, PaperParams, Pen, Pencil, PencilParams, Pe
 
 
 class Commands(StrEnum):
-    ADD = "ADD"
-    PRINT = "PRINT"
-    REM = "REM"
+	ADD = "ADD"
+	PRINT = "PRINT"
+	REM = "REM"
 
 
 def read_commands_from_file(path: str):
-    with open(path) as commands_file:
-        line = commands_file.readline()
-        while line:
-            print(f"Команда на входе: {line}")
-            parse_command_line(line)
-            line = commands_file.readline()
+	with open(path) as commands_file:
+		line = commands_file.readline()
+		while line:
+			print(f"Команда на входе: {line}")
+			parse_command_line(line)
+			line = commands_file.readline()
+
 
 def is_valid_command(command: str):
-    return command in Commands
+	return command in Commands
 
 
 def is_all_params_in_enum(params: dict, enum) -> bool:
-    for parameter in params.keys():
-        if parameter not in enum:
-            return False
-    
-    return True
+	for parameter in params.keys():
+		if parameter not in enum:
+			return False
+
+	return True
+
 
 def is_valid_parameters_for_stationery(
-        params: dict,
-        stationery_type: str,
-        stationery_enum: StrEnum
+	params: dict, stationery_type: str, stationery_enum: StrEnum
 ):
-    if params["type"] == stationery_type:
-        params.pop("type")
-        if is_all_params_in_enum(params, stationery_enum):
-            print(f"Объект {stationery_type} успешно считан!")
-            return True
-        else:
-            print(f"""Ошибка, неверные параметры для объекта {stationery_type}.
+	if params["type"] == stationery_type:
+		params.pop("type")
+		if is_all_params_in_enum(params, stationery_enum):
+			print(f"Объект {stationery_type} успешно считан!")
+			return True
+		else:
+			print(f"""Ошибка, неверные параметры для объекта {stationery_type}.
                   Допустимые параметры: {[param.value for param in stationery_enum]}""")
-            return False
+			return False
+
 
 def add_command_handler(parameters):
-    params_decoded: dict = json.loads(parameters)
+	params_decoded: dict = json.loads(parameters)
 
-    if is_valid_parameters_for_stationery(params_decoded, "pencil", PencilParams):
-        new_obj = Pencil(**params_decoded)
-        if new_obj:
-            all_stationeries.append(new_obj)
-            print("Объект pencil успешно добавлен в коллекцию!\n")
-        
-        return 
-    
-    if is_valid_parameters_for_stationery(params_decoded, "pen", PenParams):
-        all_stationeries.append(Pen(**params_decoded))
-        
-        print("Объект pen успешно добавлен в коллекцию!\n")
+	if is_valid_parameters_for_stationery(params_decoded, "pencil", PencilParams):
+		new_obj = Pencil(**params_decoded)
+		if new_obj:
+			all_stationeries.append(new_obj)
+			print("Объект pencil успешно добавлен в коллекцию!\n")
 
-        return 
-    
-    if is_valid_parameters_for_stationery(params_decoded, "paper", PaperParams):
-        all_stationeries.append(Paper(**params_decoded))
-        
-        print("Объект paper успешно добавлен в коллекцию!\n")
+		return
 
-        return
+	if is_valid_parameters_for_stationery(params_decoded, "pen", PenParams):
+		all_stationeries.append(Pen(**params_decoded))
+
+		print("Объект pen успешно добавлен в коллекцию!\n")
+
+		return
+
+	if is_valid_parameters_for_stationery(params_decoded, "paper", PaperParams):
+		all_stationeries.append(Paper(**params_decoded))
+
+		print("Объект paper успешно добавлен в коллекцию!\n")
+
+		return
+
 
 def print_command_handler():
-    print(f"Содержимое коллекции: {"пусто" if len(all_stationeries) == 0 else ""}")
-    for st in all_stationeries:
-        print(st)
+	print(f"Содержимое коллекции: {'пусто' if len(all_stationeries) == 0 else ''}")
+	for st in all_stationeries:
+		print(st)
+
 
 def rem_command_handler(parameters: str):
-    splitted_params = parameters.split()
+	splitted_params = parameters.split()
 
-    if len(splitted_params) != 3:
-        print("Неверные данные для условия")
-    
-    left_op, condition, right_op = splitted_params
-    all_st = all_stationeries[:]
-    for st in all_st:
-        try:
-            left_op, condition, right_op = splitted_params
-            left_op = getattr(st, left_op)
-            right_op = getattr(st, right_op)
-            
-            if not (isinstance(left_op, (int, float))
-                    and
-                    isinstance(right_op, (int, float))):
-                print(f"""ОШИБКА! Операнды должны быть целыми числами.
+	if len(splitted_params) != 3:
+		print("Неверные данные для условия")
+
+	left_op, condition, right_op = splitted_params
+	all_st = all_stationeries[:]
+	for st in all_st:
+		try:
+			left_op, condition, right_op = splitted_params
+			left_op = getattr(st, left_op)
+			right_op = getattr(st, right_op)
+
+			if not (isinstance(left_op, (int, float)) and isinstance(right_op, (int, float))):
+				print(f"""ОШИБКА! Операнды должны быть целыми числами.
                       Сейчас сравниваются -> {left_op} и {right_op}""")
-                return
-            
-            if eval(left_op + condition + right_op):
-                all_stationeries.remove(st)
-                print(f"УДАЛЕНО -> {st}")
-            
-        except AttributeError:
-            if eval(str(left_op) + str(condition) + str(right_op)):
-                all_stationeries.remove(st)
-                print(f"УДАЛЕНО -> {st}")
+				return
+
+			if eval(left_op + condition + right_op):
+				all_stationeries.remove(st)
+				print(f"УДАЛЕНО -> {st}")
+
+		except AttributeError:
+			if eval(str(left_op) + str(condition) + str(right_op)):
+				all_stationeries.remove(st)
+				print(f"УДАЛЕНО -> {st}")
 
 
 def parse_command_line(command_line: str):
-    splitted_line = command_line.split(" ", 1)
-    command, parameters = "", ""
+	splitted_line = command_line.split(" ", 1)
+	command, parameters = "", ""
 
-    if len(splitted_line) == 2:
-        command = splitted_line[0].removesuffix("\n")
-        parameters = splitted_line[1]
-    elif len(splitted_line) == 1:
-        command = splitted_line[0].removesuffix("\n")
-    else:
-        print("Неверная команда!")
+	if len(splitted_line) == 2:
+		command = splitted_line[0].removesuffix("\n")
+		parameters = splitted_line[1]
+	elif len(splitted_line) == 1:
+		command = splitted_line[0].removesuffix("\n")
+	else:
+		print("Неверная команда!")
 
-    if not is_valid_command(command):
-        print("Неверная команда!")
-    
-    if command == Commands.ADD:
-        add_command_handler(parameters)  
-    if command == Commands.PRINT:
-        print_command_handler()
-    if command == Commands.REM:
-        rem_command_handler(parameters)
+	if not is_valid_command(command):
+		print("Неверная команда!")
+
+	if command == Commands.ADD:
+		add_command_handler(parameters)
+	if command == Commands.PRINT:
+		print_command_handler()
+	if command == Commands.REM:
+		rem_command_handler(parameters)
 
 
 all_stationeries = list()
 
 if __name__ == "__main__":
-    read_commands_from_file("commands.txt")
+	read_commands_from_file("commands.txt")
--- stationery_classes.py
+++ stationery_classes.py
@@ -3,233 +3,223 @@ from enum import StrEnum
 
 
 class PencilColors(StrEnum):
-    RED = "красный"
-    BLUE = "синий"
-    ORANGE = "оранжевый"
-    VIOLET = "фиолетовый"
-    YELLOW = "желтый"
-    GREEN = "зеленый"
-    LIGHT_BLUE = "голубой"
+	RED = "красный"
+	BLUE = "синий"
+	ORANGE = "оранжевый"
+	VIOLET = "фиолетовый"
+	YELLOW = "желтый"
+	GREEN = "зеленый"
+	LIGHT_BLUE = "голубой"
+
 
 class PenTypes(StrEnum):
-    BALL = "шариковая"
-    GEL = "гелевая"
+	BALL = "шариковая"
+	GEL = "гелевая"
+
 
 class StationeryParams(StrEnum):
-    TYPE = "type"
+	TYPE = "type"
+
 
 class PencilParams(StrEnum):
-    TYPE = "type"
-    LEAD_DENCITY = "lead_density"
-    COLOR = "color"
-    PHONE_NUMBER = "phone_number"
-    PRICE = "price"
+	TYPE = "type"
+	LEAD_DENCITY = "lead_density"
+	COLOR = "color"
+	PHONE_NUMBER = "phone_number"
+	PRICE = "price"
+
 
 class PenParams(StrEnum):
-    ROD_DIAMETER = "rod_diameter"
-    PEN_TYPE = "pen_type"
-    PHONE_NUMBER = "phone_number"
-    PRICE = "price"
+	ROD_DIAMETER = "rod_diameter"
+	PEN_TYPE = "pen_type"
+	PHONE_NUMBER = "phone_number"
+	PRICE = "price"
+
 
 class PaperParams(StrEnum):
-    HEIGHT = "height"
-    WIDTH = "width"
-    WEIGHT = "weight"
-    PHONE_NUMBER = "phone_number"
-    PRICE = "price"
+	HEIGHT = "height"
+	WIDTH = "width"
+	WEIGHT = "weight"
+	PHONE_NUMBER = "phone_number"
+	PRICE = "price"
+
 
 class Stationery:
-    def __init__(self, price: float, phone_number: str):
-        self.price = price
-        self.phone_number = phone_number
-        
-    @staticmethod
-    def _is_valid_str_param_of_child_enum(param: str, child_enum: StrEnum) -> bool:
-        return isinstance(param, str) and param in child_enum
-    
-    @staticmethod
-    def _is_positive_int_param_of_child(param: int):
-        return isinstance(param, int) and param > 0
-    
-    @staticmethod
-    def _is_positive_float_param_of_child(param: float):
-        return isinstance(param, float) and param > 0.0
-    
-    @staticmethod
-    def _is_valid_phone_number(phone_number: str):
-        result = (isinstance(phone_number, str) and
-                  phone_number.startswith("+7") and
-                  len(phone_number) == 12)
-        return result
+	def __init__(self, price: float, phone_number: str):
+		self.price = price
+		self.phone_number = phone_number
 
+	@staticmethod
+	def _is_valid_str_param_of_child_enum(param: str, child_enum: StrEnum) -> bool:
+		return isinstance(param, str) and param in child_enum
+
+	@staticmethod
+	def _is_positive_int_param_of_child(param: int):
+		return isinstance(param, int) and param > 0
+
+	@staticmethod
+	def _is_positive_float_param_of_child(param: float):
+		return isinstance(param, float) and param > 0.0
+
+	@staticmethod
+	def _is_valid_phone_number(phone_number: str):
+		result = (
+			isinstance(phone_number, str)
+			and phone_number.startswith("+7")
+			and len(phone_number) == 12
+		)
+		return result
 
 
 class Pencil(Stationery):
-    def __new__(cls, *args, **kwargs):
-        if args:
-            kwargs["lead_density"] = args[0]
-            kwargs["color"] = args[1]
+	def __new__(cls, *args, **kwargs):
+		if args:
+			kwargs["lead_density"] = args[0]
+			kwargs["color"] = args[1]
 
-        if not (cls._is_valid_initial_parameters(kwargs)):
-            logging.error(f"""Неверные данные для инициализации!
+		if not (cls._is_valid_initial_parameters(kwargs)):
+			logging.error(f"""Неверные данные для инициализации!
                           Объект с данными {kwargs} не был создан из-за ошибки!""")
-            return None
+			return None
 
-        return super().__new__(cls)
-    
-    def __init__(self, color : str, lead_density: int, price: float, phone_number: str):
-        self.lead_density = lead_density
-        self.color = color
-        super().__init__(price, phone_number)
-    
+		return super().__new__(cls)
 
-    @classmethod
-    def _is_valid_initial_parameters(cls, parameters: dict) -> bool:
-        if not (super()._is_valid_str_param_of_child_enum(
-            parameters.get("color"),
-            PencilColors
-        )):
-            logging.error(f"""Параметр цвета (color) задан неверно.
+	def __init__(self, color: str, lead_density: int, price: float, phone_number: str):
+		self.lead_density = lead_density
+		self.color = color
+		super().__init__(price, phone_number)
+
+	@classmethod
+	def _is_valid_initial_parameters(cls, parameters: dict) -> bool:
+		if not (super()._is_valid_str_param_of_child_enum(parameters.get("color"), PencilColors)):
+			logging.error(f"""Параметр цвета (color) задан неверно.
                           Допустимые значения: {list(PencilColors)}.
                           Переданное значение: {parameters.get("color")}""")
-            return False
-        
-        if not super()._is_positive_int_param_of_child(parameters.get("lead_density")):
-            logging.error("""Параметр плотности грифеля (lead_density) задан неверно.
+			return False
+
+		if not super()._is_positive_int_param_of_child(parameters.get("lead_density")):
+			logging.error("""Параметр плотности грифеля (lead_density) задан неверно.
             Плотность грифеля (lead_density) должна быть целым положительным числом""")
-            return False
-        
-        if not (super()._is_positive_float_param_of_child(parameters.get("price"))):
-            logging.error("""Параметр цены (price) задан неверно.
+			return False
+
+		if not (super()._is_positive_float_param_of_child(parameters.get("price"))):
+			logging.error("""Параметр цены (price) задан неверно.
             Цена должна быть вещественным положительным числом""")
-            return False
-        
-        if not super()._is_valid_phone_number(parameters.get("phone_number")):
-            logging.error("""Номер телефона (phone_number) указан неверно.
+			return False
+
+		if not super()._is_valid_phone_number(parameters.get("phone_number")):
+			logging.error("""Номер телефона (phone_number) указан неверно.
                           Он должен начинаться с +7 и
                           составлять 12 символов (включая знак +)""")
-            return False
-        
-        return True
+			return False
 
-    def __str__(self):
-        return f"""Объект pencil. Параметры --> lead_density: {self.lead_density},
+		return True
+
+	def __str__(self):
+		return f"""Объект pencil. Параметры --> lead_density: {self.lead_density},
         color: {self.color}, price: {self.price},
         phone_number: {self.phone_number}\n"""
 
+
 class Pen(Stationery):
-    available_pen_types = ("шариковая", "гелевая")
+	available_pen_types = ("шариковая", "гелевая")
 
-    def __new__(cls, *args, **kwargs):
-        if not (cls._is_valid_initial_parameters(kwargs)):
-            logging.error(f"""Неверные данные для инициализации!
+	def __new__(cls, *args, **kwargs):
+		if not (cls._is_valid_initial_parameters(kwargs)):
+			logging.error(f"""Неверные данные для инициализации!
                           Объект с данными {kwargs} не был создан из-за ошибки!""")
-            return None
+			return None
 
-        return super().__new__(cls)
-    
-    def __init__(
-            self,
-            pen_type : str,
-            rod_diameter: float,
-            price: float,
-            phone_number: str
-    ):
-        self.rod_diameter = rod_diameter
-        self.pen_type = pen_type
-        super().__init__(price, phone_number)
+		return super().__new__(cls)
 
-    
-    @classmethod
-    def _is_valid_initial_parameters(cls, parameters: dict) -> bool:
-        if not (super()._is_valid_str_param_of_child_enum(
-            parameters.get("pen_type"),
-            cls.available_pen_types
-        )):
-            logging.error(f"""Параметр типа ручки (pen_type) задан неверно.
+	def __init__(self, pen_type: str, rod_diameter: float, price: float, phone_number: str):
+		self.rod_diameter = rod_diameter
+		self.pen_type = pen_type
+		super().__init__(price, phone_number)
+
+	@classmethod
+	def _is_valid_initial_parameters(cls, parameters: dict) -> bool:
+		if not (
+			super()._is_valid_str_param_of_child_enum(
+				parameters.get("pen_type"), cls.available_pen_types
+			)
+		):
+			logging.error(f"""Параметр типа ручки (pen_type) задан неверно.
                           Допустимые значения: {cls.available_pen_types}.
                           Переданное значение: {parameters.get("pen_type")}""")
-            return False
-        
-        if not super()._is_positive_float_param_of_child(
-            parameters.get("rod_diameter")
-        ):
-            logging.error("""Параметр диаметра (rod_diameter) стержня задан неверно.
+			return False
+
+		if not super()._is_positive_float_param_of_child(parameters.get("rod_diameter")):
+			logging.error("""Параметр диаметра (rod_diameter) стержня задан неверно.
                           Диаметр должен быть вещественным положительным числом""")
-            return False
-        
-        if not (super()._is_positive_float_param_of_child(parameters.get("price"))):
-            logging.error("""Параметр цены (price) задан неверно.
+			return False
+
+		if not (super()._is_positive_float_param_of_child(parameters.get("price"))):
+			logging.error("""Параметр цены (price) задан неверно.
                           Цена должна быть вещественным положительным числом""")
-            return False
-        
-        if not super()._is_valid_phone_number(parameters.get("phone_number")):
-            logging.error("""Номер телефона (phone_number) указан неверно.
+			return False
+
+		if not super()._is_valid_phone_number(parameters.get("phone_number")):
+			logging.error("""Номер телефона (phone_number) указан неверно.
                           Он должен начинаться с +7 и
                           составлять 12 символов (включая знак +)""")
-            return False
-        
-        return True
-    
-    def __str__(self):
-        return f"""Объект Pen. Параметры --> rod_diameter: {self.rod_diameter},
+			return False
+
+		return True
+
+	def __str__(self):
+		return f"""Объект Pen. Параметры --> rod_diameter: {self.rod_diameter},
         pen_type: {self.pen_type}, price: {self.price},
         phone_number: {self.phone_number}\n"""
 
+
 class Paper(Stationery):
-    def __new__(cls, *args, **kwargs):
-        if not (cls._is_valid_initial_parameters(kwargs)):
-            logging.error(f"""Неверные данные для инициализации!
+	def __new__(cls, *args, **kwargs):
+		if not (cls._is_valid_initial_parameters(kwargs)):
+			logging.error(f"""Неверные данные для инициализации!
                           Объект с данными {kwargs} не был создан из-за ошибки!""")
-            return None
+			return None
 
-        return super().__new__(cls)
-    
-    def __init__(
-            self,
-            weight: int,
-            height: int,
-            width: int,
-            price: float,
-            phone_number: str
-    ):
-        self.weight = weight
-        self.height = height
-        self.width = width
-        super().__init__(price, phone_number)
+		return super().__new__(cls)
 
-    @classmethod
-    def _is_valid_initial_parameters(cls, parameters: dict) -> bool:
-        if not (super()._is_positive_int_param_of_child(parameters.get("weight"))):
-            logging.error("""Параметр плотности бумаги (weight) указан неверно.
+	def __init__(self, weight: int, height: int, width: int, price: float, phone_number: str):
+		self.weight = weight
+		self.height = height
+		self.width = width
+		super().__init__(price, phone_number)
+
+	@classmethod
+	def _is_valid_initial_parameters(cls, parameters: dict) -> bool:
+		if not (super()._is_positive_int_param_of_child(parameters.get("weight"))):
+			logging.error("""Параметр плотности бумаги (weight) указан неверно.
                           Плотность должна быть целым положительным числом""")
-            return False
-        
-        if not (super()._is_positive_int_param_of_child(parameters.get("height"))):
-            logging.error("""Параметр высоты бумаги (height) указан неверно.
-                          Высота должна быть целым положительным числом""")
-            return False
+			return False
 
-        if not (super()._is_positive_int_param_of_child(parameters.get("width"))):
-            logging.error("""Параметр ширины бумаги (width) указан неверно.
+		if not (super()._is_positive_int_param_of_child(parameters.get("height"))):
+			logging.error("""Параметр высоты бумаги (height) указан неверно.
+                          Высота должна быть целым положительным числом""")
+			return False
+
+		if not (super()._is_positive_int_param_of_child(parameters.get("width"))):
+			logging.error("""Параметр ширины бумаги (width) указан неверно.
                           Ширина должна быть целым положительным числом""")
-            return False
-        
-        if not (super()._is_positive_float_param_of_child(parameters.get("price"))):
-            logging.error("""Параметр цены (price) задан неверно.
+			return False
+
+		if not (super()._is_positive_float_param_of_child(parameters.get("price"))):
+			logging.error("""Параметр цены (price) задан неверно.
                           Цена должна быть вещественным положительным числом""")
-            return False
-        
-        if not super()._is_valid_phone_number(parameters.get("phone_number")):
-            logging.error("""Номер телефона (phone_number) указан неверно.
+			return False
+
+		if not super()._is_valid_phone_number(parameters.get("phone_number")):
+			logging.error("""Номер телефона (phone_number) указан неверно.
                           Он должен начинаться с +7 и
                           составлять 12 символов (включая знак +)""")
-            return False
+			return False
 
-        return True
+		return True
 
-    def __str__(self):
-        return f"""Объект Paper. Параметры --> weight: {self.weight},
+	def __str__(self):
+		return f"""Объект Paper. Параметры --> weight: {self.weight},
         height: {self.height},
         width: {self.width},
         price: {self.price},
